edd9004df866953d17a257c87e11c09031d8090d
Vinícius Andrade <vinicius802010@hotmail.com>
2022-02-24 14:29:05 -0300

    Starting the API documentation
    Adding swagger as dependency for the project, but don't finished yet.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ const porta = 3333
 const database = require('./config/db')
 const express = require('express')
 const bodyParser = require('body-parser')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocs = require('../swagger.json')
 const app = express()
 const controllerLivros = require('./controller/livros.controller')
 
 //Creat database, if don't exist.
 database.sync() 
 
+app.use('/doc', swaggerUi.serve, swaggerUi.setup)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/livros', controllerLivros)
 
