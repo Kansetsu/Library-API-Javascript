bb7d640af614b48517ccdbf07612f63cbed45296
Vinícius Andrade <vinicius802010@hotmail.com>
2022-02-25 14:12:37 -0300

    Update documentation
    Change informations on documentation and using more functions of swagger autogen.

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -3,6 +3,38 @@ const swaggerAutogen = require('swagger-autogen')()
 const outputFile = './swagger_output.json'
 const controllerFile = ['./src/controller/livros.controller.js']
 
-//Generate a new swagger_output.json if use on terminal npm run swagger-autogen
-//for generate a documentation for this project automatic
-swaggerAutogen(outputFile, controllerFile)
+// Responsible to gerate this informations on swagger_output.json
+const doc = {
+    info: {
+        version: "1.0.0",
+        title: "Library API",
+        description: "With this API you can create and safe your books",
+        contact: {
+            email: "vmartins@daitan.com"
+        }
+    },
+    host: "localhost:3333",
+    basePath: "/livros",
+    schemes: ["http"],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    definitions: {
+        Book: {
+            nome: "Cinderella",
+            autor: "Patrícia Amorim",
+            editora: "Vale das letras",
+            preco: 20.93,
+            descricao: "A Editora Vale das Letras traz para você uma forma fácil e divertida de incentivar suas crianças a praticar a língua inglesa!",
+            categoria: "Infantil",
+            dataPublicacao: "2019-03-13",
+            idioma: "Português",
+            numeroDePaginas: 36
+        }
+    }
+}
+
+
+//Generate a new swagger_output.json when you start the project, starting index.js (npm run swagger-autogen)
+swaggerAutogen(outputFile, controllerFile, doc).then(() => {
+    require('../index.js')
+})
--- src/controller/livros.controller.js
+++ src/controller/livros.controller.js
@@ -4,22 +4,53 @@ const serviceLivro = require('../services/livro.service')
 
 
 controller.post('/' , (req, res)=> { 
+    // #swagger.tags = ['Library']
+    // #swagger.summary = 'Create a book'
+    // #swagger.description = 'This route will be responsible for create a book in database'
+    /* #swagger.parameters['book'] = {
+        in: 'body',
+        description: 'Book information',
+        required: true,
+        schema: { $ref: "#/definitions/Book"}
+    }*/
     res.send(serviceLivro.create(req))
 })
 
 controller.get('/', async (req, res) => {
+    // #swagger.tags = ['Library']    
+    // #swagger.summary = 'Get all books'
+    // #swagger.description = 'Get all books'
     res.send(await serviceLivro.getAll())
 })
 
 controller.get('/:id', async(req, res) => {
-    res.send(await serviceLivro.getByID(req.params.id))        
+    // #swagger.tags = ['Library']
+    // #swagger.summary = 'Search for a book with your ID'
+    // #swagger.description = 'Search for a book with your ID'
+    // #swagger.parameters['id'] = { description: 'User ID' }    
+    res.send(await serviceLivro.getByID(req.params.id))      
 })
 
 controller.put('/:id', async(req, res) =>{
+    // #swagger.tags = ['Library']
+    // #swagger.summary = 'Update a book'
+    // #swagger.description = 'Update data from a book using your ID'
+    // #swagger.parameters['id'] = { description: 'User ID' }
+    /* #swagger.parameters['book'] = {
+        in: 'body',
+        description: 'Book information for update',
+        required: true,
+        schema: { $ref: "#/definitions/Book"}
+    }*/    
     res.send(await serviceLivro.update(req.params.id, req.body))
 })
 
 controller.delete('/:id', async(req, res) =>{
+    // #swagger.summary = 'Delete a book'
+    // #swagger.description = 'Delete a book in database'
+    // #swagger.tags = ['Library']
+    // #swagger.parameters['id'] = { description: 'User ID'}    
     res.send(await serviceLivro.delete(req.params.id))
 })
+
 module.exports = controller
